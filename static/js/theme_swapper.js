c73c1c560a61e434a0cf7e6a0c8ddfa41796a7cc
MustafinMP <mustafinmp@yandex.ru>
2024-08-17 16:00:27 +0500

    Fix header style

--- static/js/theme_swapper.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector('.theme-button');
-let page = document.querySelector('.page');
-button.onclick = function() {
-  page.classList.toggle('theme-light');
-  page.classList.toggle('theme-dark');
-  console.log("Click");
-};
-
