d8d0c3b088ec1d2fea809ab4205bbc8ef18262bb
MustafinMP <mustafinmp@yandex.ru>
2024-08-26 15:29:48 +0500

    Add models for Team version

--- backend/teams/models.py
+++ backend/teams/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Integer, Column, String, ForeignKey
+from sqlalchemy import String, ForeignKey, Table, Column, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db_session import SqlAlchemyBase
 
@@ -6,17 +7,26 @@ from db_session import SqlAlchemyBase
 class Team(SqlAlchemyBase):
     __tablename__ = 'team'
 
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
-    name: str = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    creator_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+    creator = relationship('User', foreign_keys=[creator_id])
 
 
-class Role(SqlAlchemyBase):
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
-    name: str = Column(String, nullable=False)
-    team: int = Column(Integer, ForeignKey('team.id'))
-    user: int = Column(Integer, ForeignKey('user.id'))
+user_to_team = Table(
+    'user_to_team', SqlAlchemyBase.metadata,
+    Column('user', Integer, ForeignKey('user.id')),
+    Column('team', Integer, ForeignKey('team.id'))
+)
 
-
-class Permission(SqlAlchemyBase):
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
-    name: str = Column(String, nullable=False)
+# class Role(SqlAlchemyBase):
+#     id: int = Column(Integer, primary_key=True, autoincrement=True)
+#     name: str = Column(String, nullable=False)
+#     team: int = Column(Integer, ForeignKey('team.id'))
+#     user: int = Column(Integer, ForeignKey('user.id'))
+#
+#
+# class Permission(SqlAlchemyBase):
+#     id: int = Column(Integer, primary_key=True, autoincrement=True)
+#     name: str = Column(String, nullable=False)
